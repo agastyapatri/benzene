b3a6280b1fa14e815cc858689995e3f868bbfc73
agastyapatri <patriagastya5@gmail.com>
2026-05-17 15:35:43 +0530

    checkpoint before the major refactor to include non-contiguous tensors in the conversation. RIP

--- main.cpp
+++ main.cpp
@@ -7,24 +7,19 @@ using tensor = bz::tensor;
 
 constexpr bz::i32 batch_size = 1;
 constexpr bz::i32 seq_len =  4;
-constexpr bz::i32 embd_dim = 768;
-constexpr bz::i32 d_kq =  8;
-constexpr bz::i32 d_v =  8;
-constexpr bz::i32 num_heads =  2;
+constexpr bz::i32 embd_dim = 10;
+
+// constexpr bz::i32 embd_dim = 768;
+// constexpr bz::i32 d_kq =  8;
+// constexpr bz::i32 d_v =  8;
+// constexpr bz::i32 num_heads =  2;
 
 int main(){
-	tensor x = tensor::randn({batch_size, seq_len, gpt2::gpt2_small_embd_dim});
-	nn::Sequential seq;
-	seq.push_back(std::make_unique<gpt2::MLPBlock>(gpt2::gpt2_small_embd_dim));
-	tensor out = seq(x);
-	for(auto i : out.shape())
-		std::cout << i << " ";
-	std::cout << std::endl;
+	tensor t1 = tensor::randn({batch_size, seq_len, embd_dim});
 
 
-	
 
 
-	
+
 	return EXIT_SUCCESS;
 }
